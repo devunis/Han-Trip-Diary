50803f07f448df37ff8125eec2737a9ddd01a7df
jungyoon huh <gjwjddbsg@gmail.com>
2020-08-28 15:22:35 +0900

    hello react

--- src/main/java/com/htd/config/SwaggerConfig.java
+++ src/main/java/com/htd/config/SwaggerConfig.java
@@ -1,19 +1,11 @@
 package com.htd.config;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
-
-import java.util.Optional;
 
 //http://localhost:8080/swagger-ui/index.html
 @Configuration
